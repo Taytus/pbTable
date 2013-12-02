0471dbcd9acfda70bfc19e6aaaaeba674de10828
pieroblunda <pieroblunda@gmail.com>
2013-12-02 13:56:04 -0300

    Version 0.5 Beta

--- pbTable.js
+++ pbTable.js
@@ -1,3 +1,84 @@
+(function($){
+	$.fn.pbTable = function(options_user){
+		var options_default = {
+			selectable: true,
+			inputID:null,
+			createSearchBox:true,
+			//tags:null
+		}
+		
+		//herencia de las opciones definidasa por el usuario
+		options = jQuery.extend(options_default, options_user);
+		
+		var myTable = $(this);
+		
+		//selectable
+		if(options.selectable){
+			$(this).children('tbody').css('cursor', 'pointer');
+			$(this).children('tbody').children('tr').on('click', function(){
+				myTable.children('tbody').children('tr').removeClass('warning');
+				$(this).toggleClass('warning');
+			});
+		}
+		
+		/*
+		if(options.tags!=null){
+			var tagsButtons = '<div class="col-lg-8">'
+			
+			for (i=0; i<options.tags.length; i++){
+				tagsButtons += '<button class="btn btn-link" input-search="'+$(this).attr('id')+'">'+options.tags[i]+'</button>'
+			}
+			
+			tagsButtons += '</div>'
+			console.log(tagsButtons);
+		}
+		*/
+		
+		//Creando un search-input
+		if(options.createSearchBox){
+			var colspan = $(this).children('tbody').children('tr:first').children('td').length;
+			var idTablaMadre = $(this).attr('id');
+			var thead = '<tr>'+
+							'<th colspan="'+colspan+'">'+
+								'<div class="row"><div class="col-lg-4">'+
+									'<input type="text" class="form-control" search-in="'+idTablaMadre+'">'+
+								'</div></div>'+
+							'</th>'+
+						'</tr>';
+			
+			if($(this).children('thead').length>0){
+				//Si tiene cabecera
+				$(this).children('thead').prepend(thead);
+			}else{
+				//Si no tiene cabecera
+				thead = '<thead>'+thead+'</thead>'
+				$(this).prepend(thead);
+			}
+		}
+		
+		//Con un input propio
+		console.log(options.inputID);
+		if(options.inputID!=null){
+			console.log('#'+options.inputID);
+			$('#'+options.inputID).attr('search-in', $(this).attr('id'));
+		}
+		
+		//Funcion de busqueda
+		if(options.inputID!=null || options.createSearchBox){
+			$('input[search-in]').keyup(function(){
+				filtrarTabla($(this), '');
+			});
+		}
+	};
+}(jQuery));
+
+//jQuery expression for case-insensitive filter
+$.extend($.expr[":"], {
+	"contains-ci": function(elem, i, match, array){
+		return (elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+	}
+});
+
 function filtrarTabla(campoBusqueda, textoBusqueda){
 	if(textoBusqueda != '')
 		campoBusqueda.val(textoBusqueda);
@@ -10,49 +91,3 @@ function filtrarTabla(campoBusqueda, textoBusqueda){
 		$("#"+miTabla+" td:contains-ci('" + campoBusqueda.val() + "')").parent("tr").show();
 	}
 }
-
-//jQuery expression for case-insensitive filter
-$.extend($.expr[":"], {
-	"contains-ci": function(elem, i, match, array){
-		return (elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-	}
-});
-
-$(document).ready(function(){
-	$('table.conFiltro').each(function(index, element){
-		var colspan = $(this).children('tbody').children('tr:first').children('td').length;
-		var idTablaMadre = $(this).attr('id');
-		var thead = '<tr>'+
-						'<th colspan="'+colspan+'">'+
-							'<div class="row"><div class="col-lg-4">'+
-								'<input type="text" class="form-control" search-in="'+idTablaMadre+'">'+
-							'</div></div>'+
-						'</th>'+
-					'</tr>';
-		if($(this).children('thead').length>0){
-			//Si tiene cabecera
-			$(this).children('thead').prepend(thead);
-		}else{
-			//Si no tiene cabecera
-			thead = '<thead>'+thead+'</thead>'
-			$(this).prepend(thead);
-		}
-	});
-	
-	$('input[search-in]').keyup(function(){
-		filtrarTabla($(this), '');
-	});
-	
-	$('.quickTag').click(function(){
-		//var campoBusqueda = document.getElementById($(this).attr('input-search'));
-		var campoBusqueda = $('#' + $(this).attr('input-search'));
-		filtrarTabla(campoBusqueda, $(this).html());
-	});
-	
-	//Toogle tr on click
-	$('table.selectable>tbody>tr').css('cursor', 'pointer');
-	$('table.selectable>tbody>tr').on('click', function(){
-		$('table.selectable>tbody>tr').removeClass("warning");
-		$(this).toggleClass("warning");
-	});
-});
